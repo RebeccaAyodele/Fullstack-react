0bd5bb107f13b3bf7ce40bc0406cdf09539ad6ae
Rebecca Ayodele <rebeccaayodele08@gmail.com>
2025-03-28 04:42:17 +0100

    Tried adding the input focus effect using useRef and useEffect but the changes is not reflecting

--- src/components/NewItemForm.tsx
+++ src/components/NewItemForm.tsx
@@ -1,19 +1,24 @@
 import { useState } from "react";
-
+import useFocus from "./hooks/useFocus";
 import { NewItemFormContainer, NewItemButton, NewItemInput } from "../styles";
 
 interface NewItemFormProps {
   onAdd(text: string): void;
 }
 
-const NewItemForm = ({onAdd}: NewItemFormProps) => {
-    const [text, setText] = useState("")
+const NewItemForm = ({ onAdd }: NewItemFormProps) => {
+  const [text, setText] = useState("");
+  const inputRef = useFocus();
   return (
     <NewItemFormContainer>
-        <NewItemInput value={text} onChange={e => setText(e.target.value)}></NewItemInput>
-        <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
+      <NewItemInput
+        ref={inputRef}
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      ></NewItemInput>
+      <NewItemButton onClick={() => onAdd(text)}>Create</NewItemButton>
     </NewItemFormContainer>
-  )
-}
+  );
+};
 
-export default NewItemForm
+export default NewItemForm;
